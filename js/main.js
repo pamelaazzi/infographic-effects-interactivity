1f667be6e6c0b622561a2b0c7c4258c681820dbe
pamelaazzi <pamelaa.1993@hotmail.com>
2016-04-29 11:20:21 -0400

    added everything in there

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$('ul').localScroll();
+
+var $body = $('.banner');
+var $pop = $('.pop');
+var $pills;
+var bannerWidth = $body.outerWidth();
+var bannerHeight = $body.outerHeight();
+
+for (i = 0; i < 40; i++) {
+  $pills = $('<div>');
+  $pills.addClass('pills');
+  $pills.css({
+    top: Math.random() * 100 + "%",
+    left: Math.random() * 100 + "%",
+    transform: 'rotate(' + Math.random() * 180 + 'deg) scale(' + Math.random() + ')'
+  });
+  $body.append($pills);
+}
+
+$pop.waypoint(function (direction) {
+  if (direction == 'down') {
+  $pop.addClass('js-pop-animate');
+  } else {
+  $pop.removeClass('js-pop-animate');
+}
+}, { offset: '50%' });
